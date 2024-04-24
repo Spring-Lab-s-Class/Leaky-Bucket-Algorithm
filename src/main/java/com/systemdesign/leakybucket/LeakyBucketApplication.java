6e82297cab4159c184819b725facaffdf36d1830
jay-so <merry9504@naver.com>
2024-04-24 23:50:46 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/systemdesign/leakybucket/LeakyBucketApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.leakybucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeakyBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeakyBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/systemdesign/leakybucket/LeakyBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.leakybucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeakyBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
